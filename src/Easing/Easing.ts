567eb08e56ab5770b1e85f95008e90bae4553e86
Alex Figliolia <alexfigliolia@gmail.com>
2024-01-21 20:27:04 -0800

    Why no line breaks

--- src/Easing/Easing.ts
+++ src/Easing/Easing.ts
@@ -2,15 +2,19 @@ export class Easing {
   public static linear(t: number, b: number, c: number, d: number) {
     return (c * t) / d + b;
   }
+
   public static ease(t: number, b: number, c: number, d: number) {
     return -c * ((t = t / d - 1) * t * t * t - 1) + b;
   }
+
   public static "ease-in"(t: number, b: number, c: number, d: number) {
     return c * (t /= d) * t * t + b;
   }
+
   public static "ease-out"(t: number, b: number, c: number, d: number) {
     return c * ((t = t / d - 1) * t * t + 1) + b;
   }
+
   public static "ease-in-out"(t: number, b: number, c: number, d: number) {
     if ((t /= d / 2) < 1) {
       return (c / 2) * t * t * t + b;
